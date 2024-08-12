f9cffa4d7e3d68a112c6cbe0d0bd2816dc1a2dda
Ricardo Chino <ricardoychino@gmail.com>
2024-08-12 11:21:53 -0300

    Base HTML

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { map } from 'leaflet'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const asd = map('map-wrapper', {
+    center: [51.505, -0.09],
+    zoom: 13
+  })
+})
+</script>
 
 <template>
   <main>
-    IP Address Tracker Search for any IP address or domain IP Address Location Timezone UTC
-    <!-- add offset value dynamically using the API -->
-    ISP
+    <header>
+      <h1>IP Address Tracker</h1>
+      <input type="text" placeholder="Search for any IP address or domain" />
+    </header>
+
+    <div id="map">
+      <div class="infos">
+        <div class="info-item">
+          <h4>IP Address</h4>
+          <span>192.212.174.101</span>
+        </div>
+        <div class="info-item">
+          <h4>Location</h4>
+          <span>Brooklyn, NY 10001</span>
+        </div>
+        <div class="info-item">
+          <h4>Timezone</h4>
+          <span>UTC -05:00</span>
+        </div>
+        <div class="info-item">
+          <h4>ISP</h4>
+          <span>SpaceX Starlink</span>
+        </div>
+      </div>
+
+      <div id="map-wrapper"></div>
+    </div>
   </main>
 </template>
 
