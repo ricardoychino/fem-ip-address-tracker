93273e99a44713d7cdeafd7e5002dc9e4db00ec9
Ricardo Chino <ricardoychino@gmail.com>
2024-08-13 10:22:54 -0300

    Composable for caching results

---
+++ src/composables/useCachedResponses.ts
@@ -0,0 +1,51 @@
+import { ref, onUnmounted, watchEffect, toValue } from 'vue';
+import type { MaybeRefOrGetter, Ref } from "vue";
+
+export type UseCachedResponsesProps<T> = {
+  add: (key: string, value: T) => void,
+  has: (key: string) => boolean,
+  get: (key: string) => T | undefined,
+  clear: () => void
+}
+
+export const useCachedResponses = <T>(key: string, initialValue?: MaybeRefOrGetter<Map<string, T>>): UseCachedResponsesProps<T> => {
+
+  const map = ref<Map<string, T>>(new Map<string, T>(JSON.parse(localStorage.getItem(key) || '[]'))) as Ref<Map<string, T>>
+
+  if (initialValue && initialValue instanceof Map) {
+    for (const [key, value] of [...initialValue.entries()]) {
+      map.value.set(key, value)
+    }
+  }
+
+  const add = (key: string, value: MaybeRefOrGetter<T>) => {
+    map.value.set(key, toValue(value))
+  }
+  const has = (key: string) => map.value.has(key)
+  const get = (key: string) => map.value.get(key)
+  const clear = () => {
+    map.value.clear()
+  }
+
+  watchEffect(() => {
+    localStorage.setItem(key, JSON.stringify([...map.value.entries()]))
+  })
+
+  const handleStorageEvent = (event: StorageEvent) => {
+    if (event.key === key) {
+      if (event.newValue === '' || event.newValue === '{}') {
+        map.value.clear()
+      } else {
+        map.value = new Map<string, T>(JSON.parse(event.newValue || '[]'))
+      }
+    }
+  }
+
+  window.addEventListener('storage', handleStorageEvent)
+
+  onUnmounted(() => {
+    window.removeEventListener('storage', handleStorageEvent)
+  })
+
+  return { add, has, get, clear }
+}
